a5152806151e8ba66d4c11e318ef0657f734e736
Nicolas Lambert <nicolas.lambert@cnrs.fr>
2021-10-01 16:35:09 +0200

    update

--- tmp.R
+++ tmp.R
@@ -1,67 +1,65 @@
 library(sf)
 
+countries <- st_read("data/world/geom/countries.gpkg")
+colnames(countries)
+countr <- countries[, c("adm0_a3_is", "label", "Code2","Label2")]
+data = read.csv("data/world/pop.csv")
+
+countr <-
+  merge(
+    x = countr,
+    y = data,
+    by.x = "adm0_a3_is",
+    by.y = "id",
+    all.x = TRUE
+  )
+
+countr[countr$adm0_a3_is == "ESH","pop2019"] <- 405210
+countr[countr$adm0_a3_is == "AIA","pop2019"] <- 15094
+countr[countr$adm0_a3_is == "GLP","pop2019"] <- 395700
+countr[countr$adm0_a3_is == "WLF","pop2019"] <- 15289
+countr[countr$adm0_a3_is == "TKL","pop2019"] <- 1411
+countr[countr$adm0_a3_is == "SPM","pop2019"] <- 5888
+countr[countr$adm0_a3_is == "SHN","pop2019"] <- 4255
+countr[countr$adm0_a3_is == "REU","pop2019"] <- 859959
+countr[countr$adm0_a3_is == "BES","pop2019"] <- 25987
+countr[countr$adm0_a3_is == "COK","pop2019"] <- 17459
+countr[countr$adm0_a3_is == "FLK","pop2019"] <- 2840
+countr[countr$adm0_a3_is == "GUF","pop2019"] <- 294071
+countr[countr$adm0_a3_is == "JEY","pop2019"] <- 164623
+countr[countr$adm0_a3_is == "MSR","pop2019"] <- 4992 
+countr[countr$adm0_a3_is == "MTQ","pop2019"] <- 376480 
+countr[countr$adm0_a3_is == "MYT","pop2019"] <- 270372 
+countr[countr$adm0_a3_is == "NIU","pop2019"] <- 1620 
+countr[countr$adm0_a3_is == "VAT","pop2019"] <- 825 
+View(countr)
+colnames(countr) <- c("id","name","id_reg","name_reg","xxx","pop2019","geometry")
+countr <- countr[, c("id","name","id_reg","name_reg","pop2019","geometry")]
+
+# crs <-
+#   "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+
+crs <-
+  "+proj=aeqd +lat_0=90 +lon_0=50 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs "
+
+countr <- st_transform(x = countr, crs = crs)
+
+st_write(countr,"world2.shp")
 
 
-library("readxl")
-library("comparator")
-library("reshape2")
-# Import et lise en forme des données
+# --------------------------------------
 
+countries <- st_read("data/world/cartogram/WorldPopulation.shp")
+grid <- st_read("data/world/cartogram/grid.shp")
+View(countries)
 
-data_url <- "https://www.un.org/en/development/desa/population/migration/data/estimates2/data/UN_MigrantStockByOriginAndDestination_2019.xlsx"
-file <- "data/world/UN_MigrantStockByOriginAndDestination_2019.xlsx"
-if (!file.exists(file)){
-download.file(url=data_url, destfile=file)
-} 
+subregions <-
+  aggregate(countries, by = list(countries$id_reg), FUN = head, 1)
+subregions <- subregions[, c("id_reg", "name_reg")]
+st_geometry(subregions) <-
+  st_cast(subregions$geometry, "MULTIPOLYGON")
+colnames(subregions) <- c("id", "label", "geometry")
+```
 
-sheet <- "Table 1"
-year <- 2019
-
-migr <- data.frame(read_excel(file, skip = 15, sheet = sheet))
-migr <- migr[migr[,1]==year,]
-
-
-# Data Cleaning
-
-migr <- migr[!is.na(migr[,6]),]
-migr <- subset(migr, select=-c(...1,...2,...5,...4, ...6,Total,Other.North,Other.South))
-colnames(migr)[1] <- "i"
-migr <- migr[order(migr[,"i"], decreasing =FALSE),]
-for (i in 2:length(colnames(migr))){migr[,i] <- as.numeric(migr[,i])}
-
-# Get ISO codes
-
-ctr <- countries[,1:3] %>% st_drop_geometry()
-ctr <- ctr[order(ctr[,"label"], decreasing =FALSE),]
-codes <- ctr$adm0_a3_is
-
-# Verification manuelle
-ctr$rows <- migr[,"i"]
-ctr$cols <- colnames(migr)[-1]
-for(i in 1:nrow(ctr)){
-  ctr$rows_test[i] = LCS(similarity = TRUE)(ctr$label[i], ctr$rows[i]) / ((nchar(ctr$label[i]) + nchar(ctr$rows[i])) / 2) * 100
-  ctr$cols_test[i] = LCS(similarity = TRUE)(ctr$label[i], ctr$cols[i]) / ((nchar(ctr$label[i]) + nchar(ctr$cols[i])) / 2) * 100
-}
-
-head(ctr[order(ctr$cols_test),c("label","rows","cols","rows_test", "cols_test")], decreasing = FALSE)
-
-# On replace les noms de lignes et de colonnes par les codes ISO
-
-rownames(migr) <- codes
-colnames(migr) <- c("i",codes)
-migr <- migr[,-1]
-
-View(migr)
-
-# On transpose la matrice
-
-migr <- t(migr)
-
-# Avec reshape2, on la convertit au format i,j,fij
-
-migr <- melt(migr)
-colnames(migr) = c("i","j","fij")
-migr <- migr[!is.na(migr$fij),]
-migr = migr[migr$fij>0,]
-migr = migr[order(migr$fij, decreasing = TRUE),]
-
+plot(st_geometry(grid))
+plot(st_geometry(countries), add= TRUE)
